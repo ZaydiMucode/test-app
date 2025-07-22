3f7d96d029d5756bbb93df24a4b943cbd3538ece
Andre <67269555+derkandre@users.noreply.github.com>
2025-07-22 23:23:31 +0800

    fix(product): form deos not redirect to update() controller
    Fix by changing method from 'put' in the form html tag to 'put'

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -9,16 +9,13 @@
 <body>
 <h1>update a Product</h1>
 
-
-
-
-<form method= "put" action={{route('product.update',['product'=> $product])}}>
+<form method="post" action={{route('product.update',['product'=> $product])}}>
     @csrf
     @method('put')
     <div>
         <label for="product_name">Product Name:</label>
         <input type="text" id="product_name" name="name" value="{{$product->name}}"required>
-    </div> 
+    </div>
     <div>
         <label for="product_quantity">Product Quantitiy:</label>
         <input type="number" id="product_quantity" name="qty" value="{{$product->qty}}" required>
